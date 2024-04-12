5ffeddc9be23316c899b4cb37cc2c8473b7f91c1
alchemistlowkey <alchemistlowkey@gmail.com>
2024-04-12 06:23:45 +0000

    Babel setup done

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+A basic Babel setup
+"""
+
+from flask import Flask, render_template
+from flask-babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """
+    Babel config class
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """
+    An Index route method for rendering the template
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
